97adfa011c6fe2e536f87974b75ab8c7a69b2871
carrascomj <carrascomurielj@gmail.com>
2021-02-11 09:56:42 +0100

    refactor: apply experimental data to ecgem

--- ecgem/experimental.py
+++ ecgem/experimental.py
@@ -3,6 +3,8 @@
 import cobra
 import pandas as pd
 
+from .model import Model
+
 
 def limit_proteins(model: cobra.Model, measurements: pd.DataFrame):
     """Apply proteomics `measurements` to `model`.
@@ -12,7 +14,7 @@ def limit_proteins(model: cobra.Model, measurements: pd.DataFrame):
 
     Parameters
     ----------
-    model: cobra.Model
+    model: cobra.Model (or ecgem.Model)
         The enzyme-constrained model.
     measurements : pd.DataFrame
         Protein abundances in mmol / gDW.
@@ -21,19 +23,22 @@ def limit_proteins(model: cobra.Model, measurements: pd.DataFrame):
     # TODO: rely on .proteins instead of naming conventions once it's impled
     for protein_id, measure in measurements.items():
         try:
-            rxn = model.reactions.get_by_id(f"prot_{protein_id}_exchange")
+            if hasattr(model, "proteins"):
+                rxn = model.proteins.get_by_id(f"prot_{protein_id}")
+            else:
+                rxn = model.reactions.get_by_id(f"prot_{protein_id}_exchange")
         except KeyError:
             pass
         else:
             # update only upper_bound (as enzymes can be unsaturated):
-            rxn.bounds = (0, measure)
+            rxn.upper_bound = measure
 
 
 def from_mmol_gDW(
     model: cobra.Model, processed_proteomics: pd.DataFrame
 ) -> cobra.Model:
     """Apply proteomics constraints to model and return the copied EC model."""
-    ec_model = model.copy()
+    ec_model = Model(model)
     limit_proteins(ec_model, processed_proteomics)
     return ec_model
 
@@ -45,13 +50,13 @@ def from_copy_number(
     stdev: pd.Series,
     vol: float,
     dens: float,
-    water: 0.3,
+    water: float,
 ) -> cobra.Model:
     """Convert `cell_copies` to mmol/gDW and apply them to `model`."""
     df = pd.DataFrame({"cell_copies": cell_copies, "CV": stdev})
     # from molecules/cell to mmol/gDW
     df["copies_upper"] = df["cell_copies"] + 0.5 * df["CV"] / 100 * df["cell_copies"]
-    df["mmol_per_cell"] = df["copies_upper"] * 1e3 / 6.022e23
+    df["mmol_per_cell"] = df["copies_upper"] / 6.022e21
     proteomics = df["mmol_per_cell"] / (vol * dens * water)
     proteomics.index = index
     return from_mmol_gDW(model, proteomics)
